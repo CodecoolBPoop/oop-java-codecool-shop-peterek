54483ea0bc68e03a06efd67da2e510741146e912
Peter Blasko <blasko.peter.hu@gmail.com>
2018-11-22 13:17:15 +0100

    add payment.html

--- src/main/java/com/codecool/shop/controller/PaymentController.java
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -9,7 +9,6 @@ import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,74 +18,42 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
-@WebServlet(urlPatterns = {"/shopping-cart"})
-public class ShoppingCartController extends HttpServlet {
+@WebServlet(urlPatterns = {"/payment"})
+public class PaymentController extends HttpServlet {
 
     ShoppingCartDao sc = ShoppingCartDaoMem.getInstance();
     ProductDao productDataStore = ProductDaoMem.getInstance();
 
     List products = new ArrayList<Product>();
     List values = new ArrayList<Integer>();
-    float fullPrice;
+    float totalPrice;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        products = new ArrayList<Product>(sc.getCart().getProducts().keySet());
-        values = new ArrayList<Integer>(sc.getCart().getProducts().values());
-
-        createFullPrice();
+        createTotalPrice();
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         context.setVariable("sc", sc.getCart().getProducts());
-        context.setVariable("price", fullPrice);
-        engine.process("product/shopping-cart.html", context, resp.getWriter());
+        context.setVariable("total", totalPrice);
+        engine.process("product/payment.html", context, resp.getWriter());
 
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        int productIndex = Integer.valueOf(req.getParameter("itemId"));
-        int type = Integer.valueOf(req.getParameter("type"));
-
+    public void createTotalPrice(){
         products = new ArrayList<Product>(sc.getCart().getProducts().keySet());
         values = new ArrayList<Integer>(sc.getCart().getProducts().values());
-
-        if(type == 1){
-
-            for(int i=0; i<products.size(); i++){
-                if(products.get(i).equals(productDataStore.find(productIndex))){
-                    int num = Integer.valueOf(values.get(i).toString());
-                    if(num > 1){
-                        sc.add(productDataStore.find(productIndex), -1);
-                    } else if (num == 1){
-                        sc.remove(productDataStore.find(productIndex));
-                    }
-                }
-            }
-        } else if (type == 2){
-            sc.remove(productDataStore.find(productIndex));
-        }
-
-    }
-
-    public void createFullPrice(){
-        products = new ArrayList<Product>(sc.getCart().getProducts().keySet());
-        values = new ArrayList<Integer>(sc.getCart().getProducts().values());
-        fullPrice = 0;
+        totalPrice = 0;
         for(int i=0; i<products.size(); i++){
             for(int j=1; j<=productDataStore.getAll().size(); j++){
                 if(products.get(i).equals(productDataStore.find(j))){
                     float price = productDataStore.find(j).getDefaultPrice();
                     int quantity = Integer.valueOf(values.get(i).toString());
-                    fullPrice += price * quantity;
+                    totalPrice += price * quantity;
                 }
             }
         }
     }
 
 }
-
