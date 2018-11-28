ece478fcbed6939956dbea3d63458afde5242520
David Szentesi <szntsd@gmail.com>
2018-11-28 10:11:22 +0100

    add to database corrected

--- src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJDBC.java
@@ -1,8 +1,13 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.config.DbConnection;
 import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Supplier;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SupplierDaoJDBC implements SupplierDao {
@@ -19,8 +24,22 @@ public class SupplierDaoJDBC implements SupplierDao {
         return instance;
     }
 
-    public static int getSupplierIdByName() {
-        //pass
+    public static int getSupplierIdByName(String name) {
+        String query = "SELECT id FROM suppliers WHERE name=?";
+        int supplierId = -1;
+        try {
+            Connection connection = DbConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, name);
+            ResultSet result = statement.executeQuery();
+            while(result.next()){
+                supplierId = result.getInt("id");
+            }
+            return supplierId;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return -1;
     }
 
     @Override
@@ -40,6 +59,16 @@ public class SupplierDaoJDBC implements SupplierDao {
 
     @Override
     public List<Supplier> getAll() {
+        return null;
+    }
+
+    private Connection getConnection(){
+        try {
+            return DbConnection.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("SQL Exception thrown");
+        }
         return null;
     }
 }
